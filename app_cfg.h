584b762b10047844b6a1c6c98211752b557725f0
Dirk <hoffmann@vmd-jena.de>
2020-10-15 18:26:17 +0200

    Mooved some defines to app_cfg.h

--- app_cfg.h
+++ app_cfg.h
@@ -15,6 +15,8 @@
    C L A S S E S
  **********************************************************************************************************************/
 
+#define DEVICE_NAME_PREFIX	"WMC-2_"
+
 /**
  * Definitions for pcb version.
  */
@@ -38,18 +40,31 @@
  */
 #define APP_CFG_UC APP_CFG_UC_ESP8266
 
-/**
- * Pin definitions for the TFT display.
- */
-#define APP_CFG_RST D2
-#define APP_CFG_DC D1
-#define APP_CFG_CS D8
+// Pin definition for TFT was don in User_Setup.h in TFT_eSPI library
 
-#if APP_CFG_PCB_VERSION == APP_CFG_PCB_VERSION_REV01
-#define APP_CFG_SCL D4
-#define APP_CFG_SDA D3
-#else
-#define APP_CFG_SCL D3
-#define APP_CFG_SDA D4
-#endif
+#define ENABLE_SERIAL_DEBUG     0 // Set to 1 to enable serial debug
+
+#define PIN_KEYBOARD_C0         2 // D4 on WEMOS D1 Mini
+#define PIN_KEYBOARD_C1         0 // D3 on WEMOS D1 Mini
+#define PIN_KEYBOARD_C2         3 // RX on WEMOS D1 Mini
+#define PIN_KEYBOARD_C3         1 // TX on WEMOS D1 Mini
+#define KEYBOARD_SCAN_TIME      60000 //12000 us
+
+#define KEYCODE_ENCODER_BTN     132 // the encoder push button is connected to the keypad to save pins
+#define KEYCODE_POWER           68
+#define KEYCODE_MENU            36
+#define KEYCODE_MODE            20
+#define KEYCODE_LEFT            9
+#define KEYCODE_RIGHT           35
+#define KEYCODE_0               4
+#define KEYCODE_1               10
+#define KEYCODE_2               2
+#define KEYCODE_3               19
+#define KEYCODE_4               6
+#define KEYCODE_5               3
+#define KEYCODE_6               11
+#define KEYCODE_7               34
+#define KEYCODE_8               17
+#define KEYCODE_9               8
+
 #endif
